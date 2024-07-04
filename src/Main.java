1b58c92cb1f9bb66c4259fd98542cab88197c2e3
Alex <magiclun@yandex.ru>
2024-07-04 18:17:14 +0300

    04.07.24

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Collection<Integer> collection = new ArrayList<>(Arrays.asList(-49, -2, -3, 44, -5, -6, -7, -8, 9, -10));
+        search(collection);
+    }
+    public static void search(Collection<Integer> collection){
+        Integer min = null;
+        Integer max = null;
+        for (Integer integer : collection) {
+
+            if (max == null){
+                max = integer;
+            }else {
+                if (integer > max) {
+                    max = integer;
+                }
+            }
+            if (min == null) {
+                min = integer;
+            }else {
+                if (integer < min) {
+                    min = integer;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+
+}
